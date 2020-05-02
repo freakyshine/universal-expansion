ac5343ef2da42a3c9fac508ae525931f9f711da2
freakyshine <p.daniel@medalu.ch>
2020-05-02 22:47:53 +0200

    Added player_control script
    Added control script for player and removed scene light because it wasn't needed.
    Added player sprite to the scene.

---
+++ UniversalExpansion/Assets/scripts/player_control.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_control : MonoBehaviour
+{
+    public float speed;
+    private Vector2 change;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
