d15d19ea20cf9700a1463af09bfcfce6b8fa73d0
freakyshine <p.daniel@medalu.ch>
2020-05-03 21:38:39 +0200

    Added first working movement

--- UniversalExpansion/Assets/scripts/player_control.cs
+++ UniversalExpansion/Assets/scripts/player_control.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class player_control : MonoBehaviour
 {
     public float speed;
+    private Rigidbody2D myRigid;
     private Vector2 change;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        change = Vector2.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-        
+        movePlayer();
+    }
+
+    void movePlayer ()
+    {
+        myRigid.MovePosition(myRigid.position + change * speed * Time.deltaTime);
     }
 }
